12cd5e1fc929b763ee4da05da29c4673d35b5937
Maxime Riche <maxime.riche.insa@gmail.com>
2023-09-07 17:57:50 +0100

    Add warning about OpenAI chat models not supporting logprobs and logprobs likely needed.

--- src/helm/proxy/clients/openai_client.py
+++ src/helm/proxy/clients/openai_client.py
@@ -1,3 +1,5 @@
+import copy
+import traceback
 from dataclasses import replace, asdict
 from typing import Any, Dict, List, Optional, cast
 
@@ -16,7 +18,9 @@ from helm.common.tokenization_request import (
 )
 from .client import Client, truncate_sequence, wrap_request_time
 
-ORIGINAL_COMPLETION_ATTRIBUTES = openai.api_resources.completion.Completion.__bases__
+ORIGINAL_COMPLETION_ATTRIBUTES = (
+    openai.api_resources.completion.Completion.__bases__
+)
 
 
 class OpenAIClient(Client):
@@ -34,7 +38,9 @@ class OpenAIClient(Client):
         self.cache = Cache(cache_config)
 
     def _is_chat_model_engine(self, model_engine: str):
-        return model_engine.startswith("gpt-3.5") or model_engine.startswith("gpt-4")
+        return model_engine.startswith("gpt-3.5") or model_engine.startswith(
+            "gpt-4"
+        )
 
     def make_request(self, request: Request) -> RequestResult:
         if self.api_key is None:
@@ -52,12 +58,16 @@ class OpenAIClient(Client):
                 # Checks that all messages have a role and some content
                 for message in request.messages:
                     if not message.get("role") or not message.get("content"):
-                        raise ValueError("All messages must have a role and content")
+                        raise ValueError(
+                            "All messages must have a role and content"
+                        )
                 # Checks that the last role is "user"
                 if request.messages[-1]["role"] != "user":
                     raise ValueError("Last message must have role 'user'")
                 if request.prompt != "":
-                    hlog("WARNING: Since message is set, prompt will be ignored")
+                    hlog(
+                        "WARNING: Since message is set, prompt will be ignored"
+                    )
             else:
                 # Convert prompt into a single message
                 # For now, put the whole prompt in a single user message, and expect the response
@@ -71,7 +81,8 @@ class OpenAIClient(Client):
                 "temperature": request.temperature,
                 "top_p": request.top_p,
                 "n": request.num_completions,
-                "stop": request.stop_sequences or None,  # API doesn't like empty list
+                "stop": request.stop_sequences
+                or None,  # API doesn't like empty list
                 # Note: Chat models may require adding an extra token to max_tokens
                 # for the internal special role token.
                 "max_tokens": request.max_tokens,
@@ -87,7 +98,8 @@ class OpenAIClient(Client):
                 "max_tokens": request.max_tokens,
                 "best_of": request.top_k_per_token,
                 "logprobs": request.top_k_per_token,
-                "stop": request.stop_sequences or None,  # API doesn't like empty list
+                "stop": request.stop_sequences
+                or None,  # API doesn't like empty list
                 "top_p": request.top_p,
                 "presence_penalty": request.presence_penalty,
                 "frequency_penalty": request.frequency_penalty,
@@ -96,8 +108,23 @@ class OpenAIClient(Client):
 
             # OpenAI doesn't let you ask for more completions than the number of
             # per-token candidates.
-            raw_request["best_of"] = max(raw_request["best_of"], raw_request["n"])
-            raw_request["logprobs"] = max(raw_request["logprobs"], raw_request["n"])
+            raw_request["best_of"] = max(
+                raw_request["best_of"], raw_request["n"]
+            )
+            raw_request["logprobs"] = max(
+                raw_request["logprobs"], raw_request["n"]
+            )
+
+        if raw_request["max_tokens"] == 0 and self._is_chat_model_engine(
+            request.model_engine
+        ):
+            print(
+                "WARNING: You are requesting an empty completion while this is not supported by the OpenAI chat API."
+            )
+            print(
+                "WARNING suite 1: You are likely trying to use a benchmark that requires logprobs while the OpenAI chat API doesn't provide that"
+            )
+            print("WARNING suite 2: raw_request =", raw_request)
 
         try:
             if request.embedding:
@@ -124,14 +151,26 @@ class OpenAIClient(Client):
                     openai.organization = self.org_id
                     openai.api_key = self.api_key
                     openai.api_base = self.api_base
-                    openai.api_resources.completion.Completion.__bases__ = ORIGINAL_COMPLETION_ATTRIBUTES
+                    openai.api_resources.completion.Completion.__bases__ = (
+                        ORIGINAL_COMPLETION_ATTRIBUTES
+                    )
                     return openai.Completion.create(**raw_request)
 
             cache_key = Client.make_cache_key(raw_request, request)
-            response, cached = self.cache.get(cache_key, wrap_request_time(do_it))
+            response, cached = self.cache.get(
+                cache_key, wrap_request_time(do_it)
+            )
         except openai.error.OpenAIError as e:
-            error: str = f"OpenAI error: {e}"
-            return RequestResult(success=False, cached=False, error=error, completions=[], embedding=[])
+            error: str = (
+                f"OpenAI error: {e}, traceback: {traceback.format_exc()}"
+            )
+            return RequestResult(
+                success=False,
+                cached=False,
+                error=error,
+                completions=[],
+                embedding=[],
+            )
 
         # If the user is requesting completions instead of an embedding, then `completions`
         # needs to be populated, and `embedding` should be an empty list and vice-versa.
@@ -147,11 +186,19 @@ class OpenAIClient(Client):
                 # The OpenAI chat completion API doesn't support echo.
                 # If `echo_prompt` is true, combine the prompt and completion.
                 raw_completion_content = raw_completion["message"]["content"]
-                text: str = request.prompt + raw_completion_content if request.echo_prompt else raw_completion_content
+                text: str = (
+                    request.prompt + raw_completion_content
+                    if request.echo_prompt
+                    else raw_completion_content
+                )
                 # The OpenAI chat completion API doesn't return us tokens or logprobs, so we tokenize ourselves.
                 tokenization_result: TokenizationRequestResult = self.tokenize(
                     TokenizationRequest(
-                        text, tokenizer="openai/" + tiktoken.encoding_for_model(request.model_engine).name
+                        text,
+                        tokenizer="openai/"
+                        + tiktoken.encoding_for_model(
+                            request.model_engine
+                        ).name,
                     )
                 )
                 # Log probs are not currently not supported by the OpenAI chat completion API, so set to 0 for now.
@@ -165,7 +212,9 @@ class OpenAIClient(Client):
                     tokens=tokens,
                     finish_reason={"reason": raw_completion["finish_reason"]},
                 )
-                completions.append(truncate_sequence(completion, request))  # Truncate the text by stop sequences
+                completions.append(
+                    truncate_sequence(completion, request)
+                )  # Truncate the text by stop sequences
         else:
             for raw_completion in response["choices"]:
                 sequence_logprob = 0
@@ -173,9 +222,17 @@ class OpenAIClient(Client):
 
                 raw_data = raw_completion["logprobs"]
                 for text, logprob, top_logprobs in zip(
-                    raw_data["tokens"], raw_data["token_logprobs"], raw_data["top_logprobs"]
+                    raw_data["tokens"],
+                    raw_data["token_logprobs"],
+                    raw_data["top_logprobs"],
                 ):
-                    tokens.append(Token(text=text, logprob=logprob or 0, top_logprobs=dict(top_logprobs or {})))
+                    tokens.append(
+                        Token(
+                            text=text,
+                            logprob=logprob or 0,
+                            top_logprobs=dict(top_logprobs or {}),
+                        )
+                    )
                     sequence_logprob += logprob or 0
                 completion = Sequence(
                     text=raw_completion["text"],
@@ -187,7 +244,12 @@ class OpenAIClient(Client):
                 # so we need to manually truncate the list of tokens.
                 # TODO: filed an issue with their support to check what the expected behavior here is.
                 completion = truncate_sequence(
-                    completion, replace(request, stop_sequences=request.stop_sequences + [OpenAIClient.END_OF_TEXT])
+                    completion,
+                    replace(
+                        request,
+                        stop_sequences=request.stop_sequences
+                        + [OpenAIClient.END_OF_TEXT],
+                    ),
                 )
                 completions.append(completion)
 
@@ -204,13 +266,16 @@ class OpenAIClient(Client):
     def _get_tokenizer_name(tokenizer: str) -> str:
         return tokenizer.split("/")[1]
 
-    def tokenize(self, request: TokenizationRequest) -> TokenizationRequestResult:
+    def tokenize(
+        self, request: TokenizationRequest
+    ) -> TokenizationRequestResult:
         # For reproducibility purposes, we use huggingface/gpt2 as the default tokenizer.
         # except for gpt-3.5 turbo models and gpt-4 that use the tiktoken library.
 
         if request.tokenizer != "openai/cl100k_base":
             raise ValueError(
-                f"{request.tokenizer} is not supported." + "Only openai/cl100k-base is supported in HELM for now."
+                f"{request.tokenizer} is not supported."
+                + "Only openai/cl100k-base is supported in HELM for now."
             )
 
         cache_key = asdict(request)
@@ -218,7 +283,9 @@ class OpenAIClient(Client):
         try:
 
             def do_it():
-                tokenizer = tiktoken.get_encoding(self._get_tokenizer_name(request.tokenizer))
+                tokenizer = tiktoken.get_encoding(
+                    self._get_tokenizer_name(request.tokenizer)
+                )
                 tokens = tokenizer.encode(request.text)
                 if not request.encode:
                     tokens = [tokenizer.decode([token]) for token in tokens]
@@ -226,20 +293,25 @@ class OpenAIClient(Client):
                     tokens = tokens[: request.max_length]
                 return {"tokens": tokens}
 
-            response, cached = self.cache.get(cache_key, wrap_request_time(do_it))
+            response, cached = self.cache.get(
+                cache_key, wrap_request_time(do_it)
+            )
 
             result = TokenizationRequestResult(
                 success=True,
                 cached=cached,
                 text=request.text,
-                tokens=[TokenizationToken(value) for value in response["tokens"]],
+                tokens=[
+                    TokenizationToken(value) for value in response["tokens"]
+                ],
                 request_time=response["request_time"],
                 error=None,
             )
             return result
         except Exception as error:
             raise ValueError(
-                f"Error encoding with tiktoken: {error}." + "You might want to consider using huggingface/gpt2."
+                f"Error encoding with tiktoken: {error}."
+                + "You might want to consider using huggingface/gpt2."
             )
 
     def decode(self, request: DecodeRequest) -> DecodeRequestResult:
@@ -248,7 +320,8 @@ class OpenAIClient(Client):
 
         if request.tokenizer != "openai/cl100k_base":
             raise ValueError(
-                f"{request.tokenizer} is not supported." + "Only openai/cl100k-base is supported in HELM for now."
+                f"{request.tokenizer} is not supported."
+                + "Only openai/cl100k-base is supported in HELM for now."
             )
 
         cache_key = asdict(request)
@@ -256,12 +329,21 @@ class OpenAIClient(Client):
         try:
 
             def do_it():
-                tokenizer = tiktoken.get_encoding(self._get_tokenizer_name(request.tokenizer))
-                tokens = [token if isinstance(token, int) else tokenizer.encode(token)[0] for token in request.tokens]
+                tokenizer = tiktoken.get_encoding(
+                    self._get_tokenizer_name(request.tokenizer)
+                )
+                tokens = [
+                    token
+                    if isinstance(token, int)
+                    else tokenizer.encode(token)[0]
+                    for token in request.tokens
+                ]
                 text = tokenizer.decode(tokens)
                 return {"text": text}
 
-            response, cached = self.cache.get(cache_key, wrap_request_time(do_it))
+            response, cached = self.cache.get(
+                cache_key, wrap_request_time(do_it)
+            )
 
             return DecodeRequestResult(
                 success=True,
@@ -272,5 +354,6 @@ class OpenAIClient(Client):
             )
         except Exception as error:
             raise ValueError(
-                f"Error decoding with tiktoken: {error}." + "You might want to consider using huggingface/gpt2."
+                f"Error decoding with tiktoken: {error}."
+                + "You might want to consider using huggingface/gpt2."
             )
